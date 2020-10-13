12765121fadfb88bbcf7a03577ac5024b30c95b2
De Schrijver Stijn <stijn.deschrijver@school.ptiz.be>
2020-10-13 14:04:05 +0200

    flamp

--- assets/js/main.js
+++ assets/js/main.js
@@ -133,15 +133,15 @@
 
   // Porfolio isotope and filter
   $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item'
+    var WeerstationIsotope = $('.Weerstation-container').isotope({
+      itemSelector: '.Weerstation-item'
     });
 
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
+    $('#Weerstation-flters li').on('click', function() {
+      $("#Weerstation-flters li").removeClass('filter-active');
       $(this).addClass('filter-active');
 
-      portfolioIsotope.isotope({
+      WeerstationIsotope.isotope({
         filter: $(this).data('filter')
       });
     });
@@ -163,8 +163,8 @@
     offset: '80%'
   });
 
-  // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
+  // Weerstation details carousel
+  $(".Weerstation-details-carousel").owlCarousel({
     autoplay: true,
     dots: true,
     loop: true,
